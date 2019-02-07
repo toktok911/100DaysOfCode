e44b507be29db48707e238214d8f7299995fd65b
Tagwa Warrag <toktok911@users.noreply.github.com>
2019-02-07 19:14:09 +0200

    Add files via upload

---
+++ 4/twitter_credentials.py
@@ -0,0 +1,4 @@
+consumer_key = "Ou7jvk0JCzgHWZDVMYXqz3hRP"
+consumer_secret = "nDKxTEe4EDNRvlrvzE7dnmBrHp6Jc9jaSQOmc65tCTgHx772rP" 
+access_token = "154965816-fAteQjXh6DGfN89pywCKo8snaJvgzPAaQ6FgUanT"
+access_token_secret = "trqvb2jJleysmeJK9PPrJEIrEBwRmXTfO0icM7jvDMGji"
